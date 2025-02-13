b9a079b0d6d1d86f7ccb40f6a804ce81f60c4ab7
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2025-02-13 23:31:02 +0530

    feat: add .gitignore, remove config variable fom rating-display and use from config, update logic of caching in reating-display

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const googleApiKey = "AIzaSyAJ9pMGaHcmOiNeHEXQLGCiJcr5k3TV4F8";
-const timeLimit = 4*60*60*1000;
+export const googleApiKey = "AIzaSyAJ9pMGaHcmOiNeHEXQLGCiJcr5k3TV4F8"; // API key
+export const timeLimit = 4 * 60 * 60 * 1000; // Cache timeout - 4 hours
